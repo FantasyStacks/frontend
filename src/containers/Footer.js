576f941fbc299231c748a1b6418a7576f481bd85
William Cory <willcory10@gmail.com>
2018-05-28 13:18:16 -0700

    create footer

---
+++ src/containers/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react' 
+import PropTypes from 'prop-types' 
+
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer" >
+        footer here
+      </div>
+    )
+  }
+}
+
+Footer.propTypes = {
+  
+}
+
+export default Footer
